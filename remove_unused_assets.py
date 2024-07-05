b22ad030f6b5b98c25115eafdfd4447a59b22ecb
zhangxianbing <zhangxianbing09@163.com>
2024-07-05 22:58:26 +0800

    refactor: :construction: wip

--- remove_unused_assets.py
+++ remove_unused_assets.py
@@ -1,41 +1,37 @@
 #!python
 import os
 import shutil
+from collections import defaultdict
 
-assets_dir = ".assets"
-trash_dir = ".trash"
-readme_path = "README.md"
-
-
-def get_referenced_files(readme_path):
-    referenced_files = []
-    with open(readme_path, "r", encoding="utf-8") as f:
-        lines = f.readlines()
-        for line in lines:
-            line = line.strip()
-            # find first ![ in line
-            r = line.find("![")
-            if r == -1:
-                continue
-            line = line[r:]
-            if line.startswith("!["):
-                file_name = line.split("]")[1].strip("()")
-                referenced_files.append(file_name)
-    return referenced_files
+ROOT_DIR = "tutorial"
+ASSETS_DIR = os.path.join(ROOT_DIR, ".assets")
+TRASH_DIR = os.path.join(ROOT_DIR, ".trash")
 
 
-def main():
-    referenced_files = get_referenced_files(readme_path)
+# 遍历 ROOT_DIR 目录下的所有 md 文件，记录每个文件被引用的次数
+_refs = defaultdict(int)
+for root, dirs, files in os.walk(ROOT_DIR):
+    for file in files:
+        if file.endswith(".md"):
+            with open(os.path.join(root, file), "r", encoding="utf-8") as f:
+                for line in f:
+                    line = line.strip()
+                    # find first ![ in line
+                    r = line.find("![")
+                    if r == -1:
+                        continue
+                    line = line[r:]
+                    if line.startswith("!["):
+                        file_name = line.split("]")[1].strip("()")
+                        file_name = file_name.removeprefix("./.assets/")
+                        _refs[file_name] += 1
 
-    if not os.path.exists(trash_dir):
-        os.makedirs(trash_dir)
 
-    for file_name in os.listdir(assets_dir):
-        file_path = os.path.join(assets_dir, file_name)
-        if os.path.isfile(file_path) and not any(file_name in referenced_file for referenced_file in referenced_files):
-            print(f"Moving {file_name} to .trash")
-            shutil.move(file_path, os.path.join(trash_dir, file_name))
+if not os.path.exists(TRASH_DIR):
+    os.makedirs(TRASH_DIR)
 
-
-if __name__ == "__main__":
-    main()
+for file_name in os.listdir(ASSETS_DIR):
+    file_path = os.path.join(ASSETS_DIR, file_name)
+    if os.path.isfile(file_path) and _refs[file_name] == 0:
+        print(f"Moving {file_name} to .trash")
+        shutil.move(file_path, os.path.join(TRASH_DIR, file_name))
